4dff91bfe0841f679c1aa1df59d1acf8e51ff55f
Muazzez <64336826+mzaydin@users.noreply.github.com>
2021-02-20 20:14:09 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,19 @@
 #Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+#Question 3 ; Day 4
+def prime_first(num):
+    temp = 0
+    for i in range(2,num-1):
+        if(num%i == 0):
+            temp += 1
+    if temp==0:
+        print(num)        
+        
+def prime_second(num):
+    prime_first(num)
+
+for i in range(2,100):
+    if(i < 500):
+        prime_first(i)
+    elif(499 < i < 1000):
+        prime_second(i)
